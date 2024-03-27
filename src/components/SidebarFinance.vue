075c604d5e22e9e645b4c131bc6462d69c045e75
SATiYA <satiya@MACBOOKs-MacBook-Pro.local>
2024-03-27 14:50:43 +0700

    update deploy npm run build

--- src/components/SidebarFinance.vue
+++ src/components/SidebarFinance.vue
@@ -2,7 +2,7 @@
     <aside class="main-sidebar sidebar-dark-success elevation-4">
         <div class="text-center">
             <a href="#" class="brand-link">
-                <img src="../../dist/img/logo_cmh.png" alt="CMPHO Logo" class="brand-image img-circle elevation-3"
+                <img src="/dist/img/logo_cmh.png" alt="CMPHO Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">One Accounting</span>
             </a>
