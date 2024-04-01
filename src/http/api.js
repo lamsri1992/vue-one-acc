82a17621487c7d82f0d2bb4bb5254f49370dfad5
SATiYA <satiya@MACBOOKs-MacBook-Pro.local>
2024-04-01 09:28:02 +0700

    update path baseURL2

--- src/http/api.js
+++ src/http/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  // baseURL: "http://localhost:8000"
+  //baseURL: import.meta.env.VITE_BASE_AUTH,
+  baseURL: "http://192.168.100.139:8000"
 });
 
 export default api;
